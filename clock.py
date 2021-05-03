f9a2c5a049cf227b84825af2a5e6c288ea9321ea
Impanaas <77835405+Impanaas@users.noreply.github.com>
2021-05-03 22:55:06 +0530

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,23 @@
+#Digital clock using tkinter
+import sys
+from tkinter import *
+import time
+
+def times():
+    current_time=time.strftime("%H:%M:%S")
+    clock.config(text=current_time)
+    clock.after(200,times)
+root=Tk()
+root.geometry("450x250")
+root.configure(bg='#80ff00')
+clock=Label(root,font=("times",50,"bold"),bg="white")
+clock.grid(row=2,column=2,pady=25,padx=100)
+times()
+
+digi =Label(root,text="Digital clock",font="times 24 bold")
+digi.grid(row=0,column=2)
+
+nota=Label(root,text=" Hours      Minutes     seconds",font="times 15 bold")
+nota.grid(row=3,column=2)
+
+root.mainloop()
